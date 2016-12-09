1937eede55f0fd1f6ca4017d5c2ea9e9fd14531e
Dimitrios Karagiannis <dhkarag@gmail.com>
2016-12-09 17:05:31 +0000

    fixes ingress tests

--- ingress_test.go
+++ ingress_test.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"reflect"
 	"sync"
 	"testing"
+	"time"
 
 	"k8s.io/client-go/1.5/kubernetes/fake"
 	"k8s.io/client-go/1.5/pkg/api"
@@ -111,9 +113,28 @@ type testIngressEvent struct {
 }
 
 func newTestIngressWatcherClient(initial ...v1beta1.Ingress) (*fake.Clientset, *watch.FakeWatcher) {
-	client := fake.NewSimpleClientset(&v1beta1.IngressList{Items: []v1beta1.Ingress(initial)})
+	client := fake.NewSimpleClientset(&v1beta1.IngressList{Items: initial})
 	watcher, _ := client.Extensions().Ingresses(api.NamespaceDefault).Watch(api.ListOptions{})
 	return client, watcher.(*watch.FakeWatcher)
+}
+
+func waitForTrue(test func() bool, timeout time.Duration) error {
+	timer := time.NewTimer(timeout)
+	ticker := time.NewTicker(timeout / 100)
+	defer func() {
+		timer.Stop()
+		ticker.Stop()
+	}()
+	for {
+		select {
+		case <-ticker.C:
+			if test() {
+				return nil
+			}
+		case <-timer.C:
+			return fmt.Errorf("timed out")
+		}
+	}
 }
 
 func TestIngressWatcher(t *testing.T) {
@@ -141,15 +162,37 @@ func TestIngressWatcher(t *testing.T) {
 		iw.Start()
 	}()
 
+	// because the events are processed asynchronously, using a wait function
+	// here guarantees that they will arrive in the correct order and enables
+	// testing for equality
+	pLenIs := func(n int) func() bool {
+		return func() bool {
+			pM.Lock()
+			defer pM.Unlock()
+			if len(processed) == n {
+				return true
+			}
+			return false
+		}
+	}
+	if err := waitForTrue(pLenIs(2), 10*time.Second); err != nil {
+		t.Fatalf("timed out waiting for ingressWatcher to process events")
+	}
 	watcher.Delete(testIngressA)
+	if err := waitForTrue(pLenIs(3), 10*time.Second); err != nil {
+		t.Fatalf("timed out waiting for ingressWatcher to process events")
+	}
 	watcher.Modify(testIngressB2)
+	if err := waitForTrue(pLenIs(4), 10*time.Second); err != nil {
+		t.Fatalf("timed out waiting for ingressWatcher to process events")
+	}
 
 	iw.Stop()
 	wg.Wait()
 
 	pM.Lock()
 	if !reflect.DeepEqual(processed, expected) {
-		t.Errorf("ingressWatcher did not produce expected results")
+		t.Errorf("ingressWatcher did not produce expected results: %+v != %+v", processed, expected)
 	}
 	pM.Unlock()
 }
