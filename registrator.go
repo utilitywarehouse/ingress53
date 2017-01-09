b6c9c189b5cba40a653dbcea416490770a6a8422
Dimitrios Karagiannis <dhkarag@gmail.com>
2017-01-09 16:57:06 +0000

    merged files

--- registrator.go
+++ registrator.go
@@ -301,3 +301,23 @@ func resolveCname(name string, nameservers []string) (string, error) {
 
 	return retTarget, retError
 }
+
+func diffStringSlices(a []string, b []string) []string {
+	ret := []string{}
+
+	for _, va := range a {
+		exists := false
+		for _, vb := range b {
+			if va == vb {
+				exists = true
+				break
+			}
+		}
+
+		if !exists {
+			ret = append(ret, va)
+		}
+	}
+
+	return ret
+}
--- registrator_test.go
+++ registrator_test.go
@@ -373,3 +373,45 @@ func TestDNSClient_ResolveCname_broken(t *testing.T) {
 		t.Fatalf("Client.ResolveA returned unexpected response")
 	}
 }
+
+func TestDiffStringSlices(t *testing.T) {
+	testCases := []struct {
+		A []string
+		B []string
+		R []string
+	}{
+		{
+			A: []string{"A"},
+			B: []string{"A"},
+			R: []string{},
+		},
+		{
+			A: []string{"A"},
+			B: []string{"B"},
+			R: []string{"A"},
+		},
+		{
+			A: []string{},
+			B: []string{"B"},
+			R: []string{},
+		},
+		{
+			A: []string{"A"},
+			B: []string{},
+			R: []string{"A"},
+		},
+		{
+			A: []string{"A", "B", "C"},
+			B: []string{"B", "D", "A"},
+			R: []string{"C"},
+		},
+	}
+
+	for i, test := range testCases {
+		r := diffStringSlices(test.A, test.B)
+		if !reflect.DeepEqual(r, test.R) {
+			t.Errorf("diffStringSlices returned unexpected result for test case #%02d: %+v", i, r)
+		}
+	}
+
+}
